e285d475ea5b4569d726317595e65938a83234e4
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-10-16 21:29:00 +0200

    Prepare for OMNI

--- custom_galaxysmtd.mk
+++ custom_galaxysmtd.mk
@@ -1,18 +1,19 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Release name
 PRODUCT_RELEASE_NAME := GalaxyS
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Add telephony product
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Inherit some common OmniROM stuff.
+$(call inherit-product, vendor/custom/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/galaxysmtd/full_galaxysmtd.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := galaxysmtd
-PRODUCT_NAME := cm_galaxysmtd
+PRODUCT_NAME := custom_galaxysmtd
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9000
 
